8c1a7287158cb9c236d36da85bed8de03dec2b85
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-27 11:31:19 -0400

    Cargando

---
+++ Practica_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<int> miPila = new Stack<int>();
+
+        miPila.Push(10);
+        miPila.Push(20);
+        miPila.Push(30);
+        miPila.Push(40);
+
+        Console.WriteLine("Contenido de la pila:");
+        foreach (int n in miPila)
+        {
+            Console.WriteLine(n);
+        }
+        Console.ReadLine();
+    }
+}
