eb05be05fd84e25f16e37167f1a29295efe2a1d5
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-27 19:39:42 -0400

    cargando

---
+++ Practica_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>();
+
+        pila.Push(1);
+        pila.Push(2);
+        pila.Push(3);
+
+        while (pila.Count > 0)
+        {
+            pila.Pop();
+        }
+
+        Console.WriteLine("La pila está vacía: " + (pila.Count == 0));
+
+        Console.ReadLine();
+    }
+}
