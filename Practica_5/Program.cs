e42497880f1f113b090b37de762e41cfe80494fc
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-27 20:23:29 -0400

    cargando

---
+++ Practica_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<int> cola = new Queue<int>();
+
+        cola.Enqueue(10);
+        cola.Enqueue(20);
+        cola.Enqueue(30);
+
+        int suma = 0;
+
+        foreach (int num in cola)
+        {
+            suma = suma + num;
+        }
+
+        Console.WriteLine("La suma es: " + suma);
+
+        Console.ReadLine();
+    }
+}
