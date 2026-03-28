d39f711e004d07312e21c5ffeafb5a78b3ce6a5d
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-03-27 20:01:09 -0400

    cargando

---
+++ Practica_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>();
+
+        pila.Push(5);
+        pila.Push(10);
+        pila.Push(15);
+
+        pila.Pop();
+        pila.Pop();
+
+        Console.WriteLine("Estado final de la pila:");
+
+        foreach (int num in pila)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.ReadLine();
+    }
+}
